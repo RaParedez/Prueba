6247acb1012d478062f479161953864641ada815
Roberto Paredes Zuñiga <rpzuniga@Robertos-MacBook-Pro.local>
2020-04-25 00:12:49 -0500

    Probar Commit

--- Prueba.js
+++ Prueba.js
@@ -30,7 +30,7 @@ fun1();
 fun2();
 */
 
-function nextInLine(arr, item){
+/*function nextInLine(arr, item){
     arr.push(item);
     return arr.shift();
 }
@@ -39,4 +39,9 @@ var testArr = [1,2,3,4,5];
 
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
-console.log("After: " + JSON.stringify(testArr));
+console.log("After: " + JSON.stringify(testArr));*/
+
+var X = "Robson";
+
+console.log(X);
+
